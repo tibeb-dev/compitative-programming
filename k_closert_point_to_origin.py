147e945e64d467dc24f4eec6e7eb5fa319efd253
Guest User <Guest@MacBook-Pro-2.local>
2022-07-09 19:38:22 +0300

    k closest points to origin

---
+++ k_closert_point_to_origin.py
@@ -0,0 +1,25 @@
+class Solution(object):
+    def kClosest(self, points, k):
+        """
+        :type points: List[List[int]]
+        :type k: int
+        :rtype: List[List[int]]
+        """
+        minHeap=[]
+        res=[]
+        for x,y in points:
+            dist=(x**2)+(y**2)
+            minHeap.append([dist,x,y])
+
+        heapq.heapify(minHeap)
+
+        while k>0:
+            dist,x,y=heapq.heappop(minHeap)
+            res.append([x,y])
+            k-=1
+        return res
+
+
+        
+
+        
