18d86d08abd08517be283d621d33c52c5d4f0b65
kidussintayehu <kidussintayehu60@gmail.com>
2022-10-27 13:44:56 +0300

    pancake sorting

---
+++ sorting/pancake_sorting.py
@@ -0,0 +1,19 @@
+class Solution:
+    def pancakeSort(self, arr: List[int]) -> List[int]:
+        n = len(A)
+        res = []
+        for i in range(n):
+		    cur_max = max(A[0:n-i])
+            j = 0
+            while A[j] != cur_max:
+                j += 1
+            # should reverse j+1 elements
+            A[:j+1] = reversed(A[:j+1])
+            res.append(j+1)
+            # reverse all
+            A[:n-i] = reversed(A[:n-i])
+            res.append(n-i)
+	    return res
+    
+    
+#     [3,2,4,1]   [4,]
